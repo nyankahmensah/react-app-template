eea7fd58a108343493d7448beeef557968c6934c
Nyankah Mensah <nyankahmensah@gmail.com>
2022-04-10 14:08:35 +0000

    fix crypto-hash bug

--- src/apollo/link/pq.ts
+++ src/apollo/link/pq.ts
@@ -1,5 +1,5 @@
 import { createPersistedQueryLink } from "@apollo/client/link/persisted-queries";
-import { sha256 } from 'crypto-hash/browser';
+import { sha256 } from 'crypto-hash';
 
 const persistedQueriesMiddleware = createPersistedQueryLink({ sha256 });
 
